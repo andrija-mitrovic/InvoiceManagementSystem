541046f0bd60aef80883eca5f4fc068ad520c992
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-10-25 09:13:24 +0200

    AuditEntity added

---
+++ InvoiceManagementSystem/src/InvoiceManagementSystem.Domain/Common/AuditEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace InvoiceManagementSystem.Domain.Common
+{
+    public class AuditEntity<T>
+    {
+        public T Id { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime Created { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime? LastModified { get; set; }
+    }
+}
