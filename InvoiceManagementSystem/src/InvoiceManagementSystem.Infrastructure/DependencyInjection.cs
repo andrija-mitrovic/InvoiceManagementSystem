3672a56a1024a0d062fe0611476f78e92f52909f
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-10-25 09:42:22 +0200

    DependencyInjection classes added into Application and Infrastructer project

---
+++ InvoiceManagementSystem/src/InvoiceManagementSystem.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using InvoiceManagementSystem.Infrastructure.Data;
+using InvoiceManagementSystem.Infrastructure.Interfaces;
+using InvoiceManagementSystem.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InvoiceManagementSystem.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(
+                configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<ICurrentUserService, CurrentUserService>();
+
+            return services;
+        }
+    }
+}
