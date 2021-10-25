7db643d62fa6ee13ffd60a10fe7162a26d40c886
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-10-25 09:13:43 +0200

    InvoiceItem added

---
+++ InvoiceManagementSystem/src/InvoiceManagementSystem.Domain/Entities/Invoice.cs
@@ -0,0 +1,24 @@
+﻿using InvoiceManagementSystem.Domain.Common;
+using InvoiceManagementSystem.Domain.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace InvoiceManagementSystem.Domain.Entities
+{
+    public class Invoice : AuditEntity<int>
+    {
+        public string InvoiceNumber { get; set; }
+        public string Logo { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public DateTime Date { get; set; }
+        public string PaymentTerms { get; set; }
+        public DateTime? DueDate { get; set; }
+        public double Discount { get; set; }
+        public DiscountType DiscountType { get; set; }
+        public double Tax { get; set; }
+        public TaxType TaxType { get; set; }
+        public double AmountPaid { get; set; }
+        public ICollection<InvoiceItem> InvoiceItems { get; set; }
+    }
+}
