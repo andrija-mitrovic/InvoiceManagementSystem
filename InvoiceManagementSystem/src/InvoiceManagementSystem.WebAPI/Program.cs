e08cb3d45ed43a0d410393f32f332100d8b14d62
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-10-26 08:56:15 +0200

    Migration added int program.cs

--- InvoiceManagementSystem/src/InvoiceManagementSystem.WebAPI/Program.cs
+++ InvoiceManagementSystem/src/InvoiceManagementSystem.WebAPI/Program.cs
@@ -1,19 +1,46 @@
+using InvoiceManagementSystem.Domain.Entities;
+using InvoiceManagementSystem.Infrastructure.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace InvoiceManagementSystem.WebAPI
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using var scope = host.Services.CreateScope();
+
+            var services = scope.ServiceProvider;
+
+            var logger = services.GetRequiredService<ILogger<Program>>();
+
+            try
+            {
+                logger.LogInformation("Program.Main - Seeding data started.");
+
+                var context = services.GetRequiredService<ApplicationDbContext>();
+                //var userManager = services.GetRequiredService<UserManager<AppUser>>();
+                await context.Database.MigrateAsync();
+               // await ApplicationDbContextSeed.SeedDataAsync(context, userManager, CancellationToken.None);
+
+                logger.LogInformation("Program.Main - Seeding data finished.");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Program.Main - An error occured during migration.");
+            }
+
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
