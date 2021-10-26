523759112be8de65cc1556cce8160c25256e729b
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-10-26 08:54:32 +0200

    Seeding class added

---
+++ InvoiceManagementSystem/src/InvoiceManagementSystem.Infrastructure/Data/ApplicationDbContextSeed.cs
@@ -0,0 +1,43 @@
+﻿using InvoiceManagementSystem.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InvoiceManagementSystem.Infrastructure.Data
+{
+    public class ApplicationDbContextSeed
+    {
+        public static async Task SeedDataAsync(ApplicationDbContext context, 
+            UserManager<AppUser> userManager, 
+            CancellationToken cancellationToken)
+        {
+            if (!userManager.Users.Any())
+            {
+                var users = new List<AppUser>
+                {
+                    new AppUser
+                    {
+                        UserName="admin",
+                        Email="admin@gmail.com"
+                    },
+                    new AppUser
+                    {
+                        UserName="andrija",
+                        Email="andrija.mitrovic9@gmail.com"
+                    }
+                };
+
+                foreach(var user in users)
+                {
+                    await userManager.CreateAsync(user, "Password");
+                }
+
+                await context.SaveChangesAsync(cancellationToken);
+            }
+        }
+    }
+}
