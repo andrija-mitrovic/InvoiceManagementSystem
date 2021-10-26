4673abcbc1e4981fcb7786cdd4dc905e29715958
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-10-26 11:08:44 +0200

    InvoiceController added

---
+++ InvoiceManagementSystem/src/InvoiceManagementSystem.WebAPI/Controllers/InvoicesController.cs
@@ -0,0 +1,17 @@
+﻿using InvoiceManagementSystem.Application.DTOs;
+using InvoiceManagementSystem.Application.Features.Invoices.Command;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InvoiceManagementSystem.WebAPI.Controllers
+{
+    public class InvoicesController : BaseApiController
+    {
+        [HttpPost]
+        public async Task<IActionResult> AddInvoice(InvoiceDto invoiceDto, CancellationToken cancellationToken)
+        {
+            return Ok(await Mediator.Send(new CreateInvoiceCommand { Invoice = invoiceDto }, cancellationToken));
+        }
+    }
+}
