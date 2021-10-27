40db72613364f06f04ab4b393f43edc6266e758c
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-10-27 08:33:58 +0200

    Configured dependencyInjection and authentication in the infrastructure project

--- InvoiceManagementSystem/src/InvoiceManagementSystem.Infrastructure/DependencyInjection.cs
+++ InvoiceManagementSystem/src/InvoiceManagementSystem.Infrastructure/DependencyInjection.cs
@@ -2,31 +2,61 @@
 using InvoiceManagementSystem.Infrastructure.Data;
 using InvoiceManagementSystem.Infrastructure.Interfaces;
 using InvoiceManagementSystem.Infrastructure.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace InvoiceManagementSystem.Infrastructure
 {
     public static class DependencyInjection
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
-        { 
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    configuration.GetConnectionString("DefaultConnection")));
-
-            //services.AddIdentityCore<AppUser>(opt =>
-            //{
-            //    opt.Password.RequireNonAlphanumeric = false;
-            //})
-            //.AddEntityFrameworkStores<ApplicationDbContext>()
-            //.AddSignInManager<SignInManager<AppUser>>();
+        {
+            ConfigureDbContext(services, configuration);
+            ConfigureIdentity(services);
+            ConfigureAuthentication(services, configuration);
 
             services.AddScoped<ICurrentUserService, CurrentUserService>();
 
             return services;
+        }
+
+        private static void ConfigureDbContext(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection")));
+        }
+
+        private static void ConfigureIdentity(IServiceCollection services)
+        {
+            services.AddIdentityCore<AppUser>(opt =>
+            {
+                opt.Password.RequireNonAlphanumeric = false;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddSignInManager<SignInManager<AppUser>>();
+        }
+
+        private static void ConfigureAuthentication(IServiceCollection services, IConfiguration configuration)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["TokenKey"]));
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(opt =>
+                {
+                    opt.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = key,
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
         }
     }
 }
