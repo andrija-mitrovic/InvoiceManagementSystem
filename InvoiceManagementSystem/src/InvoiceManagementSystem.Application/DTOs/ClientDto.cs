4eaceec6f23e95ec14b4c98def833e83226a48f6
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-10-28 08:49:29 +0200

    ClientDto added

---
+++ InvoiceManagementSystem/src/InvoiceManagementSystem.Application/DTOs/ClientDto.cs
@@ -0,0 +1,10 @@
+﻿namespace InvoiceManagementSystem.Application.DTOs
+{
+    public class ClientDto
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
