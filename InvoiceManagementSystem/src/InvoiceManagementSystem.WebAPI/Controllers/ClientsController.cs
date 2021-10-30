5abfd41298e22219e5b9d6db7e94d59a0b1bf94e
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-10-30 10:04:19 +0200

    ClientController added

---
+++ InvoiceManagementSystem/src/InvoiceManagementSystem.WebAPI/Controllers/ClientsController.cs
@@ -0,0 +1,42 @@
+﻿using InvoiceManagementSystem.Application.DTOs;
+using InvoiceManagementSystem.Application.Features.Clients.Command;
+using InvoiceManagementSystem.Application.Features.Clients.Queries;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InvoiceManagementSystem.WebAPI.Controllers
+{
+    public class ClientsController : BaseApiController
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetClients(CancellationToken cancellationToken)
+        {
+            return Ok(await Mediator.Send(new GetClientListQuery(), cancellationToken));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetClient(int id, CancellationToken cancellationToken)
+        {
+            return Ok(await Mediator.Send(new GetClientByIdQuery { Id = id }, cancellationToken));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateClient(ClientDto clientDto, CancellationToken cancellationToken)
+        {
+            return Ok(await Mediator.Send(new CreateClientCommand { Client = clientDto }, cancellationToken));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateClient(int id, ClientDto clientDto, CancellationToken cancellationToken)
+        {
+            return Ok(await Mediator.Send(new EditClientCommand { Id = id, Client = clientDto }, cancellationToken));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteClient(int id, CancellationToken cancellationToken)
+        {
+            return Ok(await Mediator.Send(new DeleteClientCommand { Id = id }, cancellationToken));
+        }
+    }
+}
