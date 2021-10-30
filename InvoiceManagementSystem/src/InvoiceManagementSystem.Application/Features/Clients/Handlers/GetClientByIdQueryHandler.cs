25591e71e5f85d1e5edabc9e2caf33d7e1284ec0
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-10-30 10:02:43 +0200

    Get client by id handler added

---
+++ InvoiceManagementSystem/src/InvoiceManagementSystem.Application/Features/Clients/Handlers/GetClientByIdQueryHandler.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using InvoiceManagementSystem.Application.DTOs;
+using InvoiceManagementSystem.Application.Features.Clients.Queries;
+using InvoiceManagementSystem.Infrastructure.Data;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InvoiceManagementSystem.Application.Features.Clients.Handlers
+{
+    public class GetClientByIdQueryHandler : IRequestHandler<GetClientByIdQuery, ClientDto>
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+        private readonly ILogger<GetClientByIdQueryHandler> _logger;
+
+        public GetClientByIdQueryHandler(ApplicationDbContext context,
+            IMapper mapper,
+            ILogger<GetClientByIdQueryHandler> logger)
+        {
+            _context = context;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task<ClientDto> Handle(GetClientByIdQuery request, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"GetClientByIdQueryHandler.Handle - Retrieved client with Id={request.Id}");
+
+            var client = await _context.Clients.FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+
+            if (client == null)
+            {
+                _logger.LogError("GetClientByIdQueryHandler.Handle - No client.");
+                return null;
+            }
+
+            _logger.LogInformation($"GetClientByIdQueryHandler.Handle - Successfully returned client with Id={request.Id}");
+            return _mapper.Map<ClientDto>(client);
+        }
+    }
+}
