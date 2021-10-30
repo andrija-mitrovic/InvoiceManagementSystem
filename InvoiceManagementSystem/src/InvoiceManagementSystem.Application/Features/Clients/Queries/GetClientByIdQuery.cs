266e8f7acf8d666c6c8c19d2f13a961e98108c62
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-10-30 10:03:33 +0200

    Gt client query added

---
+++ InvoiceManagementSystem/src/InvoiceManagementSystem.Application/Features/Clients/Queries/GetClientByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using InvoiceManagementSystem.Application.DTOs;
+using MediatR;
+
+namespace InvoiceManagementSystem.Application.Features.Clients.Queries
+{
+    public class GetClientByIdQuery : IRequest<ClientDto>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ InvoiceManagementSystem/src/InvoiceManagementSystem.Application/Features/Clients/Queries/GetClientListQuery.cs
@@ -0,0 +1,10 @@
+﻿using InvoiceManagementSystem.Application.DTOs;
+using MediatR;
+using System.Collections.Generic;
+
+namespace InvoiceManagementSystem.Application.Features.Clients.Queries
+{
+    public class GetClientListQuery : IRequest<List<ClientDto>>
+    {
+    }
+}
