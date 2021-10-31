c6ae022790ae65174fcf84a33aa086a7fa4536f3
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-10-31 09:06:13 +0100

    Create company handler added

---
+++ InvoiceManagementSystem/src/InvoiceManagementSystem.Application/Features/CompanyInfo/Command/CreateCompanyInfoCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace InvoiceManagementSystem.Application.Features.CompanyInfo.Command
+{
+    public class CreateCompanyInfoCommand : IRequest<Unit>
+    {
+        public Domain.Entities.CompanyInfo CompanyInfo { get; set; }
+    }
+}
---
+++ InvoiceManagementSystem/src/InvoiceManagementSystem.Application/Features/CompanyInfo/Handlers/CreateCompanyInfoCommandHanlder.cs
@@ -0,0 +1,40 @@
+﻿using InvoiceManagementSystem.Application.Features.CompanyInfo.Command;
+using InvoiceManagementSystem.Infrastructure.Data;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InvoiceManagementSystem.Application.Features.CompanyInfo.Handlers
+{
+    public class CreateCompanyInfoCommandHanlder : IRequestHandler<CreateCompanyInfoCommand, Unit>
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<CreateCompanyInfoCommandHanlder> _logger;
+
+        public CreateCompanyInfoCommandHanlder(ApplicationDbContext context,
+            ILogger<CreateCompanyInfoCommandHanlder> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public async Task<Unit> Handle(CreateCompanyInfoCommand request, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("CreateCompanyInfoCommandHanlder.Handle - Adding company information.");
+
+            _context.CompanyInfo.Add(request.CompanyInfo);
+
+            var result = await _context.SaveChangesAsync(cancellationToken) > 0;
+
+            if (!result)
+            {
+                _logger.LogError("CreateCompanyInfoCommandHanlder.Handle - Failed to add company information.");
+                return Unit.Value;
+            }
+
+            _logger.LogInformation("CreateCompanyInfoCommandHanlder.Handle - Successfully added company information.");
+            return Unit.Value;
+        }
+    }
+}
