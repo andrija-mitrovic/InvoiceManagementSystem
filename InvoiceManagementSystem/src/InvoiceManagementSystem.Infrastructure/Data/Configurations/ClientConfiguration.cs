8f298c3851e4bffecde1a108a7fa7b63af6eb3e7
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-11-17 07:34:11 +0100

    Configured client entity

---
+++ InvoiceManagementSystem/src/InvoiceManagementSystem.Infrastructure/Data/Configurations/ClientConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using InvoiceManagementSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InvoiceManagementSystem.Infrastructure.Data.Configurations
+{
+    public class ClientConfiguration : IEntityTypeConfiguration<Client>
+    {
+        public void Configure(EntityTypeBuilder<Client> builder)
+        {
+            builder.Property(c => c.Name).HasMaxLength(255);
+            builder.Property(c => c.Address).HasMaxLength(255);
+            builder.Property(c => c.PhoneNumber).HasMaxLength(50);
+            builder.Property(c => c.Email).HasMaxLength(40);
+        }
+    }
+}
