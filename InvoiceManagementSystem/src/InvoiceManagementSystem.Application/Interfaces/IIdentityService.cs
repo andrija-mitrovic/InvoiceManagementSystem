c661f75e17208ec9b6b33357d8f7e11abed79314
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-11-17 07:33:52 +0100

    Identity service added

---
+++ InvoiceManagementSystem/src/InvoiceManagementSystem.Application/Interfaces/IIdentityService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace InvoiceManagementSystem.Application.Interfaces
+{
+    public interface IIdentityService
+    {
+        Task<string> GetUserNameAsync(string userId);
+
+        Task<bool> IsInRoleAsync(string userId, string role);
+
+        Task<bool> AuthorizeAsync(string userId, string policyName);
+    }
+}
