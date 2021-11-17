da47b267da90095c194df5078abc0a28c6d5b687
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-11-17 07:34:39 +0100

    Configured invoice entity

---
+++ InvoiceManagementSystem/src/InvoiceManagementSystem.Infrastructure/Data/Configurations/InvoiceConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using InvoiceManagementSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InvoiceManagementSystem.Infrastructure.Data.Configurations
+{
+    public class InvoiceConfiguration : IEntityTypeConfiguration<Invoice>
+    {
+        public void Configure(EntityTypeBuilder<Invoice> builder)
+        {
+            builder.Property(c => c.InvoiceNumber).HasMaxLength(255);
+            builder.Property(c => c.Logo).HasMaxLength(255);
+            builder.Property(c => c.From).HasMaxLength(50);
+            builder.Property(c => c.To).HasMaxLength(50);
+        }
+    }
+}
